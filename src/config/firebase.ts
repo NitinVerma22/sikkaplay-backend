30d8ceb14cc446682bc77a7e86e68cd6ddbe8508
eliteassignment-commits <eliteassignment@gmail.com>
2026-05-27 00:08:45 +0530

    Fix firebase json path for render

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,8 @@
 import * as admin from 'firebase-admin';
 import path from 'path';
 
-// Using the provided service account key
-const serviceAccountPath = path.resolve(__dirname, 'firebase-service-account.json');
+// Using the provided service account key from the src directory (since tsc doesn't copy json)
+const serviceAccountPath = path.resolve(process.cwd(), 'src/config/firebase-service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountPath),
