d5007694bcb112e43300bbd94e6b4500614bdc4a
eliteassignment-commits <eliteassignment@gmail.com>
2026-09-17 15:03:46 +0530

    fix: add trust proxy 1

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ import Redis from 'ioredis';
 import { MatchmakingService } from './services/matchmaking.service';
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 startCronJobs();
